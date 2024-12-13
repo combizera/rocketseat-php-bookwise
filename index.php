9ef5bc4a45512e8a47fe7b18da60fee6895b2fa3
combizera <ycombi349@gmail.com>
2024-12-13 08:53:43 -0300

    feat: styles and create page book

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    global $books;
+    require 'data.php';
+?>
 <!doctype html>
 <html lang="pt-br">
 <head>
@@ -9,13 +13,13 @@
 <body class="bg-stone-950 text-stone-300">
     <header class="bg-stone-900">
         <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
-            <div class="font-bold text-xl tracking-wide">Book Wise</div>
+            <a href="/index.php" class="font-bold text-xl tracking-wide">Book Wise</a>
             <ul class="flex space-x-4 font-bold">
                 <li>
                     <a href="/" class="text-lime-500">Explorar</a>
                 </li>
                 <li>
-                    <a href="/meus-livros.php" class="hover:underline">Meus livros</a>
+                    <a href="/my-books.php" class="hover:underline">Meus livros</a>
                 </li>
             </ul>
             <ul>
@@ -37,9 +41,35 @@
             <button type="submit">🔍</button>
         </form>
 
-        <div>
-            Lista Final
-        </div>
+        <section class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            <!--LIVRO-->
+            <?php foreach($books as $book): ?>
+                <div class="p-2 rounded border-stone-800 bg-stone-900 border-2">
+                    <div class="flex">
+                        <div class="w-1/3">
+                            img
+                        </div>
+                        <div class="space-y-1">
+                            <a
+                                href="/book.php?id=<?= $book['id'] ?>"
+                                class="font-semibold hover:underline"
+                            >
+                                <?= $book['title'] ?>
+                            </a>
+                            <div class="font-xs italic">
+                                <?= $book['author'] ?>
+                            </div>
+                            <div class="text-xs italic">
+                                star (3 avaliações)
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-sm mt-4">
+                        <?= $book['description'] ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </section>
     </main>
 </body>
 </html>
